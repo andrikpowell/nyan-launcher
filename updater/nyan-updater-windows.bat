d04c64b3a8f3361f9e69dd47a0b57e78e4fcad1e
Arsinikk <61759217+andrikpowell@users.noreply.github.com>
2026-02-20 17:37:24 -0600

    Update Nyan windows script

--- updater/nyan-updater-windows.bat
+++ updater/nyan-updater-windows.bat
@@ -1,46 +1,56 @@
 @ECHO OFF
+setlocal
 
-set REPO=andrikpowell/nyan-doom
+set "REPO=andrikpowell/nyan-doom"
 
-set DEST="%~dp0"
-if not [%1]==[] set DEST="%1"
-echo Destination folder is %DEST%
+set "DEST=%~dp0"
+if not "%~1"=="" set "DEST=%~1"
+echo Destination folder is "%DEST%"
 
 :: Get latest version
-for /f %%i in ('powershell -command "(Invoke-WebRequest https://api.github.com/repos/%REPO%/releases | ConvertFrom-Json)[0].tag_name"') do set TAG=%%i
+for /f %%i in ('powershell -NoProfile -command "(Invoke-WebRequest https://api.github.com/repos/%REPO%/releases | ConvertFrom-Json)[0].tag_name"') do set "TAG=%%i"
+if errorlevel 1 goto :fail
+
 echo Latest Nyan Doom version is %TAG%
-set VERSION=%TAG:~1%
+set "VERSION=%TAG:~1%"
 
 :: File name
-set v_win64=nyan-doom-%VERSION%-win-x64
+set "v_win64=nyan-doom-%VERSION%-win-x64"
 
-mkdir "%temp%\nyan-doom-temp"
+set "TMPDIR=%temp%\nyan-doom-temp"
+mkdir "%TMPDIR%" 2>nul
 
 :: Download zip
 echo Downloading latest release
-powershell -command "Invoke-WebRequest -OutFile \"%temp%\nyan-doom-temp\%v_win64%.zip\" -Uri https://github.com/%REPO%/releases/download/%TAG%/%v_win64%.zip"
+powershell -NoProfile -Command "Invoke-WebRequest -OutFile '%TMPDIR%\%v_win64%.zip' -Uri 'https://github.com/%REPO%/releases/download/%TAG%/%v_win64%.zip'"
+if errorlevel 1 goto :fail
+if not exist "%TMPDIR%\%v_win64%.zip" goto :fail
 
 :: Extract zip
-if exist "%temp%\nyan-doom-temp\%v_win64%.zip" (
-  echo Extracting files
-  powershell -command "Expand-Archive -Force \"%temp%\nyan-doom-temp\%v_win64%.zip\" \"%temp%\nyan-doom-temp\""
-) else (
-  echo "Error downloading update"
-  del /q "%temp%\nyan-doom-temp\%v_win64%.zip"
-  rmdir /s /q "%temp%\nyan-doom-temp\%v_win64%"
-  pause
-  exit
-)
+echo Extracting files
+powershell -NoProfile -Command "Expand-Archive -Force '%TMPDIR%\%v_win64%.zip' '%TMPDIR%'"
+if errorlevel 1 goto :fail
 
 :: Copy to destination folder
 echo Copying files to destination
-robocopy "%temp%\nyan-doom-temp\%v_win64%" "%DEST%" * /nfl /ndl /njh /njs /nc /ns /np
+robocopy "%TMPDIR%\%v_win64%" "%DEST%" * /nfl /ndl /njh /njs /nc /ns /np
+set "RC=%ERRORLEVEL%"
+if %RC% GEQ 8 goto :fail
 
 :: Delete temp files
-del /q "%temp%\nyan-doom-temp\%v_win64%.zip"
-rmdir /s /q "%temp%\nyan-doom-temp\%v_win64%"
+rmdir /s /q "%TMPDIR%" 2>nul
 
-echo Nyan Doom %VERSION% has been installed succesfully!
+echo Nyan Doom %VERSION% has been installed successfully!
+exit /b 0
+
+:: Failure? - do pause
+:fail
+echo.
+echo Update failed.
+echo.
+
+:: Best-effort cleanup
+if defined TMPDIR if exist "%TMPDIR%" rmdir /s /q "%TMPDIR%" 2>nul
 
 pause
-exit
+exit /b 1
