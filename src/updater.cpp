d06c7e4e6c47b62bf6072a4b96d3f7f6eff88c7c
Arsinikk <me@andrikpowell.com>
2026-02-22 21:45:23 -0600

    how about this

--- src/updater.cpp
+++ src/updater.cpp
@@ -227,8 +227,7 @@ void updateGame()
     process.setProgram("cmd.exe");
     process.setArguments({"/c", "powershell -command New-Item \"%temp%\\nyan-doom-temp\" -type directory -force && "
                                 "powershell -command (Invoke-WebRequest -OutFile '%temp%\\nyan-doom-temp\\nyan-updater-windows.bat' -Uri " + GAME_UPDATER_WINDOWS + ") && " +
-                                "powershell -command Start-Process -FilePath '%temp%\\nyan-doom-temp\\nyan-updater-windows.bat' -WorkingDirectory '" + launcherfolder + "' -Wait -NoNewWindow"});
-    process.startDetached();
+                                "powershell -command Start-Process -FilePath '%temp%\\nyan-doom-temp\\nyan-updater-windows.bat' -WorkingDirectory '" + launcherfolder + "' -ArgumentList . -Wait -NoNewWindow"});    process.startDetached();
 #elif defined(Q_OS_LINUX)
     QDesktopServices::openUrl(QUrl(GAME_REPO + "/releases/latest"));
 #endif
