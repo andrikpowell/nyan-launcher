385b44837e0f159e956b016b9021ac1d742d2b1e
Arsinikk <me@andrikpowell.com>
2026-09-17 17:51:47 -0500

    Revert some tweaks

--- src/updater.cpp
+++ src/updater.cpp
@@ -224,11 +224,10 @@ void updateGame()
                                                   args->startupInfo->dwFlags &= ~STARTF_USESTDHANDLES;
                                                   args->startupInfo->dwFlags |= STARTF_USEFILLATTRIBUTE;
                                               });
-    process.setWorkingDirectory(launcherfolder);
     process.setProgram("cmd.exe");
     process.setArguments({"/c", "powershell -command New-Item \"%temp%\\nyan-doom-temp\" -type directory -force && "
                                 "powershell -command (Invoke-WebRequest -OutFile '%temp%\\nyan-doom-temp\\nyan-updater-windows.bat' -Uri " + GAME_UPDATER_WINDOWS + ") && " +
-                                "call \"%temp%\\nyan-doom-temp\\nyan-updater-windows.bat\" \".\""});
+                                "powershell -command Start-Process -FilePath '%temp%\\nyan-doom-temp\\nyan-updater-windows.bat' -WorkingDirectory '" + launcherfolder + "' -ArgumentList . -Wait -NoNewWindow"});
     process.startDetached();
 #elif defined(Q_OS_LINUX)
     QDesktopServices::openUrl(QUrl(GAME_REPO + "/releases/latest"));
